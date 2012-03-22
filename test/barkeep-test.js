b1c31c7d950fc4b1c420b1daefa84a53b55f2a49
Clay Smith <smithclay@gmail.com>
2012-03-22 16:16:11 -0700

    add s3 to barkeep

--- test/barkeep-test.js
+++ test/barkeep-test.js
@@ -11,4 +11,20 @@ vows.describe('barkeep').addBatch({
             assert.instanceOf(barkeep, require('events').EventEmitter);
         }
     }
+}).addBatch({
+    "Barkeep S3": {
+        "should be able to create a client": function () {
+            Barkeep.s3.createClient({accessKeyId: "AKIAIYACYTHS3QYSLHTQ", 
+                secretAccessKey: "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"});
+            Barkeep.s3.setBucket("asset-qa.postapp.com");
+            
+            assert.isNotNull(Barkeep.s3.client);
+            assert.isNotNull(Barkeep.s3.bucket);
+        },
+        "requires access keys" : function () {
+            assert.throws(function () {
+                Barkeep.s3.createClient()
+                }, /Error: Must specify/);
+        }
+    }
 }).export(module);
