846851e8393489ad0375b01c00f9baf00ef0431b
Clay Smith <smithclay@gmail.com>
2012-03-22 16:19:25 -0700

    updated test

--- test/barkeep-test.js
+++ test/barkeep-test.js
@@ -14,9 +14,9 @@ vows.describe('barkeep').addBatch({
 }).addBatch({
     "Barkeep S3": {
         "should be able to create a client": function () {
-            Barkeep.s3.createClient({accessKeyId: "AKIAIYACYTHS3QYSLHTQ", 
-                secretAccessKey: "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"});
-            Barkeep.s3.setBucket("asset-qa.postapp.com");
+            Barkeep.s3.createClient({accessKeyId: "bar", 
+                secretAccessKey: "foo"});
+            Barkeep.s3.setBucket("bucket");
             
             assert.isNotNull(Barkeep.s3.client);
             assert.isNotNull(Barkeep.s3.bucket);
